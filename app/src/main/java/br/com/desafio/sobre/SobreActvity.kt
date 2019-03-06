d066028ef746e09e5766e055c7a110a19e5227cd
Enzo Teles <enzo.carvalho.teles@gmail.com>
2019-03-06 11:10:31 -0300

    implementando a tela de sobre

--- app/src/main/java/br/com/desafio/sobre/SobreActvity.kt
+++ app/src/main/java/br/com/desafio/sobre/SobreActvity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import br.com.desafio.R
 import br.com.desafio.base.BaseActivity
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.sobre_activity.*
 
 /**
  * Created by Enzo Teles on 06/03/19
@@ -16,6 +18,12 @@ class SobreActvity: BaseActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sobre_activity)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        toolbar_sobre.title = "sobre"
+        setSupportActionBar(toolbar_sobre)
     }
 
 }
